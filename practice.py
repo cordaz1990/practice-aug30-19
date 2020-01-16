37d11ad4defd386a2f27cd5ed68cf3d5f86c88be
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:12:43 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
-print('hello')
-print('world')
-print('world')
+def increment(self, seconds):
+    seconds += self.time_to_int()
+    return int_to_time(seconds)
+  
+ def is_after(self, other):
+     return self.time_to_int() > other.time_to_int()
+  
+  end.is_after(start)
